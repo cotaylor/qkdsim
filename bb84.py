236d2cc9afd8b5a3c7b2ad344746a94850e6a96d
Conner Taylor <cotaylor@mines.edu>
2017-04-15 03:30:59 -0700

    Implemented BB84 up to shared key without eavesdropping

--- bb84.py
+++ bb84.py
@@ -24,20 +24,32 @@ def bb84Noiseless(n, verbose=True):
 
     # Alice also randomly chooses which basis to use when encoding each bit
     # 0: computational basis; 1: Hadamard basis
-    aBases = getRandomBits(n)
+    bases_A = getRandomBits(n)
 
     if verbose:
         print("Alice's raw key:\n%s") % bitFormat(rawKey)
-        print("Alice's basis choices:\n%s") % bitFormat(aBases)
+        print("Alice's basis choices:\n%s") % bitFormat(bases_A)
 
     # Alice prepares n qubits, with the kth qubit in state |0> or |1> in either the computational
     # basis or the Hadamard basis, depending on the value of the kth bit in each bitstring
-    sent = encodeRawKey(rawKey, aBases)
+    sent_A = encodeRawKey(rawKey, bases_A)
 
     # Alice sends each qubit one at a time to Bob, who measures each in a randomly chosen basis
-    bBases = getRandomBits(n)
-    bKey = []
+    bases_B = getRandomBits(n)
+    key_B = []
     for k in range(n):
-        bKey.append(decodeState(sent[k], bBases[k]))
+        key_B.append(decodeState(sent_A[k], bases_B[k]))
 
-    if verbose: print("Bob's measurement results:\n%s") % bitFormat(bKey)
+    if verbose:
+        print("Bob's basis choices:\n%s") % bitFormat(bases_B)
+        print("Bob's measurement results:\n%s") % bitFormat(key_B)
+
+    # Bob announces when he has measured the final qubit and discloses the bases he used for
+    # each measurement. Alice and Bob then discard any bits where they chose different bases.
+    key_A, key_B = matchKeys(rawKey, key_B, bases_A, bases_B)
+
+    print("Alice's key after discarding mismatches:\n%s") % bitFormat(key_A)
+    print("Bob's key after discarding mismatches:\n%s") % bitFormat(key_B)
+
+    if (key_A == key_B): print("equal")
+    else: print("not equal")
--- utils.py
+++ utils.py
@@ -1,10 +1,14 @@
 import numpy as np
 import qit
 
+def bitFormat(bits):
+    """Return a printable representation of the given list of bools."""
+    return '[' + ', '.join(['1' if elem else '0' for elem in bits]) + ']'
+
 def decodeState(state, basis):
     """Return a bool corresponding to the result of measuring the given state in the given basis."""
     # Change basis if necessary
-    if basis: state.u_propagate(qit.H)
+    if basis: state = state.u_propagate(qit.H)
 
     # Measure in the chosen basis
     _, result = state.measure()
@@ -15,10 +19,10 @@ def encodeRawKey(key, bases):
     """Return a list of quantum states corresponding to individual qubits prepared using the
     following encoding:
          key | bases | state
-          0  |   0   | |0>
-          0  |   1   | 0.5|0> + 0.5|1>
-          1  |   0   | |1>
-          1  |   1   | 0.5|0> - 0.5|1>
+          0  |   0   | +1 |0>
+          0  |   1   | +0.7071 |0> +0.7071 |1>
+          1  |   0   | +1 |1>
+          1  |   1   | +0.7071 |0> -0.7071 |1>
 
     key and bases are lists of bools representing binary values
     """
@@ -46,6 +50,12 @@ def getRandomBits(n):
     bitstring = [elem > 0.5 for elem in dist]
     return bitstring
 
-def bitFormat(bits):
-    """Return a printable representation of the given list of bools."""
-    return '[' + ', '.join(['1' if elem else '0' for elem in bits]) + ']'
+def matchKeys(key1, key2, bases1, bases2):
+    """If bases1[k] != bases2[k], discard bit k from both keys.
+    Returns a tuple containing the resulting keys.
+    """
+    match = np.logical_not(np.logical_xor(bases1, bases2))
+    newKey1 = [key1[k] for k in range(len(key1)) if match[k]]
+    newKey2 = [key2[k] for k in range(len(key1)) if match[k]]
+
+    return (newKey1, newKey2)
