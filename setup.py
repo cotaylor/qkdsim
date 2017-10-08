083531205d7bca3ed621bd50efb681b9e2bcca05
Conner Taylor <cmtaylor15@gmail.com>
2017-10-08 18:32:18 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='qkdsim',
+      version='1.0',
+      description='Quantum Key Distribution Simulation Library',
+      author='Conner Taylor',
+      author_email='cmtaylor15@gmail.com',
+      url='https://github.com/cotaylor/qkdsim',
+      packages=['qkdsim']
+      )
+      
