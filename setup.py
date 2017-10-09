2d564b92576e52b54abd525bbadfed529ede1353
Conner Taylor <cmtaylor15@gmail.com>
2017-10-08 20:41:15 -0500

    Modify setup.py to not rely on requirements.txt

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 from distutils.core import setup
 from pip.req import parse_requirements
 
-# get requirements for install_requires
-install_reqs = parse_requirements("requirements.txt")
-reqs = [str(ir.req) for ir in install_reqs]
-
 setup(name='qkdsim',
       version='1.0',
       description='Quantum Key Distribution Simulation Library',
@@ -12,6 +8,9 @@ setup(name='qkdsim',
       author_email='cmtaylor15@gmail.com',
       url='https://github.com/cotaylor/qkdsim',
       packages=['qkdsim'],
-      install_requires=reqs
+      install_requires=[
+          'qit',
+          'pycrypto'
+          ]
       )
       
