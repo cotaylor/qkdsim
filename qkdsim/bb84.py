44879533970533e895fb43216e2fc98b78abaf7c
Conner Taylor <cmtaylor15@gmail.com>
2017-10-17 19:09:41 -0500

    Moved printing to bb84, TODO: make bb84 modular

--- qkdsim/bb84.py
+++ qkdsim/bb84.py
@@ -76,3 +76,74 @@ def matchKeys(key1, key2, bases1, bases2):
     newKey1 = [key1[k] for k in range(len(key1)) if match[k]]
     newKey2 = [key2[k] for k in range(len(key1)) if match[k]]
     return (newKey1, newKey2)
+
+## PRINT FUNCTIONS - where?
+# TODO: this is a little awkward
+def printStage0(numInitialBits, numTargetBits, eve, errorRate):
+    # Initialization
+    print("\n=====BB84 protocol=====\n%d initial bits, ~%d key bits" % (numInitialBits, numTargetBits))
+    if eve: print("with eavesdropping")
+    else: print("without eavesdropping")
+    if errorRate: print("with channel noise")
+    else: print("without channel noise")
+
+def printStage1(numBits, rawKey, bases, verbose):
+    # Raw key/basis generation
+    print("\nAlice generates %d random bits to be encoded:\n%s" % (numBits, util.bitFormat(rawKey)))
+    print("For each bit, Alice randomly chooses one of two non-orthogonal sets of bases:\n%s" % util.bitFormat(bases))
+
+    if verbose:
+        print("\nAlice encodes each bit according to the following strategy:"\
+              "\n    value | basis | state"\
+              "\n      0   |   0   | +1 |0>"\
+              "\n      0   |   1   | +0.7071 (|0> + |1>)"\
+              "\n      1   |   0   | +1 |1>"\
+              "\n      1   |   1   | +0.7071 (|0> - |1>)"\
+              "\nShe then sends each qubit one by one to Bob over a quantum channel.\n")
+
+def printStage2(numBits, bases_E, verbose):
+    # Eavesdropping
+    if verbose:
+        print("Eve intercepts each qubit as it travels to Bob. Because it is not possible"\
+                  "\nto clone quantum states, she must measure each qubit before re-sending to"\
+                  "\nBob. Every time she measures a qubit in the 'wrong' basis, she has a 50%"\
+                  "\nprobability of being detected.\n")
+
+        print("Eve chooses a random basis to measure each qubit in:\n%s" % util.bitFormat(bases_E))
+
+        if verbose: print("\nEve attempts to hide her actions by re-encoding her measurement result"\
+                          "\nbefore re-sending the qubits to Bob.\n")
+
+def printStage3(bases_B, key_B):
+    # Bob's measurements
+    print("Bob chooses a random basis to measure each qubit in:\n%s" % util.bitFormat(bases_B))
+    print("Bob's measurement results:\n%s" % util.bitFormat(key_B))    
+
+def printStage4(key_A, key_B, verbose):
+    # Discard mismatches
+    if verbose:
+        print("\nBob announces when he has measured the last qubit and discloses"\
+              "\nthe bases he used for each measurement. Alice and Bob then discard"\
+              "\nany bits where they chose different bases.\n")
+
+    print("Alice's key after discarding mismatches:\n%s" % util.bitFormat(key_A))
+    print("Bob's key after discarding mismatches:\n%s" % util.bitFormat(key_B))        
+
+def printStage5_1(numBits, announce_A, announce_B, verbose):
+    # Sacrifice subset of keys
+    if verbose:
+        print("\nAlice and Bob sacrifice %d of their %d shared bits and publicly announce"\
+              "\ntheir values. They agree to disclose every other bit of their shared key.\n" % (len(announce_A), numBits))
+
+    print("Alice's announced bits:\n%s" % util.bitFormat(announce_A))
+    print("Bob's announced bits:\n%s" % util.bitFormat(announce_B))
+
+def printStage5_2(expectedError, actualError):
+    # Error rate
+    print("Expected error rate: %f" % expectedError)
+    print("Actual error rate: %f" % actualError)
+
+def printFinalKeys(numBits, key_A, key_B):
+    # Compare final keys
+    print("Alice's remaining %d-bit key:\n%s" % (numBits, util.bitFormat(key_A)))
+    print("Bob's remaining %d-bit key:\n%s" % (numBits, util.bitFormat(key_B)))
--- qkdsim/simulations.py
+++ qkdsim/simulations.py
@@ -9,59 +9,38 @@ def runBB84(n, eve=False, errorRate=0.0, verbose=True):
     If eve is set to True, assumes the presence of an eavesdropper attempting an
     intercept-resend attack.
     """
+    ##### STAGE 1: RAWKEY AND BASIS GENERATION #####
     numBits = 5 * n
-
-    if verbose:
-        print("\n=====BB84 protocol=====\n%d initial bits, ~%d key bits" % (numBits, n))
-        if eve: print("with eavesdropping")
-        else: print("without eavesdropping")
-        if errorRate: print("with channel noise")
-        else: print("without channel noise")
-
+    if verbose: bb84.printStage0(numBits, n, eve, errorRate)
+        
     # Alice generates a random bit string to be encoded
     rawKey = util.getRandomBits(numBits)
 
     # Alice also randomly chooses which basis to use when encoding each bit
     # 0: computational basis; 1: Hadamard basis
     bases_A = util.getRandomBits(numBits)
-
-    print("\nAlice generates %d random bits to be encoded:\n%s" % (numBits, util.bitFormat(rawKey)))
-    print("For each bit, Alice randomly chooses one of two non-orthogonal sets of bases:\n%s" % util.bitFormat(bases_A))
-
-    if verbose:
-        print("\nAlice encodes each bit according to the following strategy:"\
-              "\n    value | basis | state"\
-              "\n      0   |   0   | +1 |0>"\
-              "\n      0   |   1   | +0.7071 (|0> + |1>)"\
-              "\n      1   |   0   | +1 |1>"\
-              "\n      1   |   1   | +0.7071 (|0> - |1>)"\
-              "\nShe then sends each qubit one by one to Bob over a quantum channel.\n")
+    bb84.printStage1(numBits, rawKey, bases_A, verbose)
 
     # Alice prepares n qubits, with the kth qubit in state |0> or |1> in either the computational
     # basis or the Hadamard basis, depending on the value of the kth bit in each bitstring
     sent_A = bb84.encodeKey(rawKey, bases_A)
-
+    ##### END STAGE 1 ######
+    
+    ##### STAGE 2: EAVESDROPPING #####
     # QKD guarantees with high probability we will detect any eavesdropping
-
     if eve:
-        if verbose:
-            print("Eve intercepts each qubit as it travels to Bob. Because it is not possible"\
-                  "\nto clone quantum states, she must measure each qubit before re-sending to"\
-                  "\nBob. Every time she measures a qubit in the 'wrong' basis, she has a 50%"\
-                  "\nprobability of being detected.\n")
-
         # No matter what strategy Eve uses to select bases, the probability she will be detected
         # is always 1-(3/4)^numBits if Alice chose her bases randomly
         bases_E = util.getRandomBits(numBits)
-        print("Eve chooses a random basis to measure each qubit in:\n%s" % util.bitFormat(bases_E))
 
         # Eve measures each qubit and attempts to cover her tracks
         for k in range(numBits):
             sent_A[k] = bb84.simulateEavesdrop(sent_A[k], bases_E[k])
 
-        if verbose: print("\nEve attempts to hide her actions by re-encoding her measurement result"\
-                          "\nbefore re-sending the qubits to Bob.\n")
+        bb84.printStage2(numBits, bases_E, verbose)
+    ##### END STAGE 2 #####
 
+    ##### STAGE 3: TRANSMISSION, MEASUREMENT #####
     # Introduce error due to noise
     sent_A = bb84.simulateNoise(sent_A, errorRate)
 
@@ -70,43 +49,33 @@ def runBB84(n, eve=False, errorRate=0.0, verbose=True):
     key_B = []
     for k in range(numBits):
         key_B.append(bb84.decodeState(sent_A[k], bases_B[k]))
+    bb84.printStage3(bases_B, key_B)
+    ##### END STAGE 3 #####
 
-    print("Bob chooses a random basis to measure each qubit in:\n%s" % util.bitFormat(bases_B))
-    print("Bob's measurement results:\n%s" % util.bitFormat(key_B))
-
+    ##### STAGE 4: KEY SIFTING #####
     # Alice and Bob discard any bits where they chose different bases.
     key_A, key_B = bb84.matchKeys(rawKey, key_B, bases_A, bases_B)
     numBits = len(key_A)
+    bb84.printStage4(key_A, key_B, verbose)
+    ##### END STAGE 4 #####
 
-    if verbose:
-        print("\nBob announces when he has measured the last qubit and discloses"\
-              "\nthe bases he used for each measurement. Alice and Bob then discard"\
-              "\nany bits where they chose different bases.\n")
-
-    print("Alice's key after discarding mismatches:\n%s" % util.bitFormat(key_A))
-    print("Bob's key after discarding mismatches:\n%s" % util.bitFormat(key_B))
-
+    ##### STAGE 5: DETECT EAVESDROPPING #####
     # Alice and Bob sacrifice a subset of their bits to try to detect Eve
     announce_A, key_A, announce_B, key_B = util.discloseHalf(key_A, key_B)
-    if verbose:
-        print("\nAlice and Bob sacrifice %d of their %d shared bits and publicly announce"\
-              "\ntheir values. They agree to disclose every other bit of their shared key.\n" % (len(announce_A), numBits))
-
-    print("Alice's announced bits:\n%s" % util.bitFormat(announce_A))
-    print("Bob's announced bits:\n%s" % util.bitFormat(announce_B))
-
+    bb84.printStage5_1(numBits, announce_A, announce_B, verbose)
+        
     numBits = len(key_A)
-    print("Alice's remaining %d-bit key:\n%s" % (numBits, util.bitFormat(key_A)))
-    print("Bob's remaining %d-bit key:\n%s" % (numBits, util.bitFormat(key_B)))
-
-    print("Expected error rate: %f" % errorRate)
-    print("Actual error rate: %f" % (float(sum([1 for k in range(len(key_A)) if key_A[k] != key_B[k]]))/len(key_A)))
-
+    actualError = float(sum([1 for k in range(len(key_A)) if key_A[k] != key_B[k]]))/len(key_A)
+    bb84.printStage5_2(errorRate, actualError)
     if util.detectEavesdrop(key_A, key_B, errorRate):
         print("\nAlice and Bob detect Eve's interference and abort the protocol.")
         return -1
+    ##### END STAGE 5 #####
+    
+    bb84.printFinalKeys(numBits, key_A, key_B)
+    return 0
+#    return key_A     # TODO: should this return a key or error code?
 
-    return key_A
 
 def runB92(n, eve=False, errorRate=0.0, verbose=True):
     """Simulation of Bennet's 1992 protocol for quantum key distribution with n initial
